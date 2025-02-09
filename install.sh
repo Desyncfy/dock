695913aa583c05122f074b290dc0daf9ce63e959
Henry <desync@desyncd.xyz>
2025-02-09 11:26:24 -0800

    fix installer

--- install.sh
+++ install.sh
@@ -5,13 +5,14 @@
 mkdir ~/.dock
 curl https://raw.githubusercontent.com/Desyncfy/dock/refs/heads/main/dock.sh > ~/.dock/dock
 chmod +x ~/.dock/dock
-echo '''Welcome to Dock!
+touch ~/.dock/packages.txt
+echo """Welcome to Dock!
 
 Next steps:
 
-1. Add ~/.dock to your Path
-Bash: export PATH="$PATH:~/.dock" (add to ~/.bashrc)
-Fish: set fish_user_paths ~/.dock $fish_user_paths
+1. Add $directory to your Path
+Bash: export PATH="\$PATH:~/.dock" (add to ~/.bashrc)
+Fish: set fish_user_paths ~/.dock \$fish_user_paths
 
 2. Install a package
-dock install <package>'''
+dock install <package>"""
